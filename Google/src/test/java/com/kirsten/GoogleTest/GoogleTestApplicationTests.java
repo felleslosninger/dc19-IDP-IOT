80240d480fe8a31af583447f02ced92a7e4d738d
matsjp <matspedersen@protonmail.com>
2019-07-16 08:33:52 +0200

    Adding Google code

---
+++ Google/src/test/java/com/kirsten/GoogleTest/GoogleTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kirsten.GoogleTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
